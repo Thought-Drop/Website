9dc898f4d7f1c3c8429c83dc0faf402a12f1195c
Justin Coleman <JustinVColeman@gmail.com>
2015-01-05 21:47:24 -0600

    Added desktop styles and cleaned up mobile styles

--- js/global.js
+++ js/global.js
@@ -8,3 +8,27 @@ $(function(){
         $(this).attr('placeholder',$(this).data('holder'));
     });
 });
+
+$(document).ready(function() {
+  setupRotator();
+});
+
+function setupRotator() {
+  if($('blockquote').length > 1) {
+    $('#quote1').addClass('current').fadeIn(1000);
+    setInterval(rotateQuote(), 5000);
+  }
+}
+
+function rotateQuote() {
+  var current = $('blockquote.current');
+  if(current.next().length === 0) {
+     current.removeClass('current').fadeOut(1000);
+     $('blockquote:first').addClass('current').fadeIn(1000);
+     console.log("IF");
+  } else {
+     current.removeClass('current').fadeOut(1000);
+     current.next().addClass('current').fadeIn(1000);
+     console.log("ELSE");
+  }
+}
